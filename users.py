4a2e942e213f12afbb844105f17f4be738f8dc71
Tiger Zhao <zhao.tiger@outlook.com>
2017-11-18 14:05:43 -0500

    test

--- users.py
+++ users.py
@@ -5,16 +5,15 @@ import stockapi
 def getAvailableFunds():
     try:
         return database.runQueryWithResponse("select availableFunds from users where name='John Doe';")[0][0]
-    except:
+    except Exception:
         return "-1"
     
 
 #returns true if success
 def updateFunds(newAmount):
-    try:
-        database.runQueryWithResponse("update users set availableFunds = '"+str(newAmount)+"' where name='John Doe';")
-    except:
-        return False
+    
+    database.runQueryWithResponse("update users set availableFunds = '"+str(newAmount)+"' where name='John Doe';")
+    
     
     
     return True
