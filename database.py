1161ca1ee800e74e8048abd3bb2b0a77d7728a43
Tiger Zhao <zhao.tiger@outlook.com>
2017-11-18 02:58:20 -0500

    updated database

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 import os
-import urlparse
+from urllib import parse 
 import psycopg2
 
 def getConn():
     urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["postgres://norlxajvyplpfe:e24415b89946199f789e67bf4538d4cd9ff9a12c53c4f1a7baeace48e029429e@ec2-23-23-249-169.compute-1.amazonaws.com:5432/d7npj38pkmgv9n"])
+    url = parse.urlparse(os.environ["postgres://norlxajvyplpfe:e24415b89946199f789e67bf4538d4cd9ff9a12c53c4f1a7baeace48e029429e@ec2-23-23-249-169.compute-1.amazonaws.com:5432/d7npj38pkmgv9n"])
 
     conn = psycopg2.connect(
         database=url.path[1:],
