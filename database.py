2bd47f6cd3d4d294f4889e0c1c9d8418ceb2e8b6
Tiger Zhao <zhao.tiger@outlook.com>
2017-11-18 02:38:27 -0500

    added endpoints and testing sql

---
+++ database.py
@@ -0,0 +1,34 @@
+import os
+import urlparse
+import psycopg2
+
+def getConn():
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["postgres://norlxajvyplpfe:e24415b89946199f789e67bf4538d4cd9ff9a12c53c4f1a7baeace48e029429e@ec2-23-23-249-169.compute-1.amazonaws.com:5432/d7npj38pkmgv9n"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+
+
+def initialize():
+    conn = getConn()
+    cur = conn.cursor()
+    cur.execute("
+        CREATE TABLE IF NOT EXISTS buys (
+            stockName varchar(48) NOT NULL,  
+            buyPrice varchar(48) NOT NULL,
+            timestamp TIMESTAMP DEFAULT NOW(),
+            enabled integer NOT NULL DEFAULT '1',  
+        )
+    ")
+
+
+
+
+
+
--- endpoints.py
+++ endpoints.py
@@ -1,4 +1,5 @@
 from flask import Flask, json, request
+import database.py 
 import os
 
 app = Flask(__name__)
@@ -22,7 +23,25 @@ def api_root():
     returnThing = {'message': 'look its a message'}
     return json.dumps(returnThing)
 
+@app.route('/stocks/buy', methods=['Post', 'Get'])
+def buyStock():
+    ret = {'message': 'buy stock is not implemented yet'}
+    return json.dumps(ret)
+
+
+@app.route('/stocks/sell', methods=['Post', 'Get'])
+def sellStock():
+    ret = {'message': 'sell stock is not implemented yet'}
+    return json.dumps(ret)
+
+
+@app.route('/stocks/data', methods =['Post', 'Get'])
+def getStockData():
+    ret = {'message': 'get stock data not implemented'}
+    return json.dumps(ret)
+
 
 if __name__ == '__main__':
+    database.initialize() 
     port = int(os.environ.get("PORT", 1337))
     app.run(debug=True, host='0.0.0.0', port=port)
