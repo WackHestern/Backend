1c55d449dbd16e781f8784de2eedbcb89cca43db
Tiger Zhao <zhao.tiger@outlook.com>
2017-11-18 14:31:51 -0500

    added sell functionality

--- stocks.py
+++ stocks.py
@@ -27,7 +27,26 @@ def buy(stockName, amount):
 
 
 def sell(stockName, amount):
-    pass
+    price = stockapi.getPrice(stockName)
+    
+    if (float(price) < 0):
+        print ("failed price check:" + str(price))
+        return "fail"
+    availFunds = availFunds = float(users.getAvailableFunds())
+    
+    curStockCnt= int(getStockCntByName(stockName))
+    amount = int(amount)
+    if curStockCnt < amount:
+        return "insufficient stocks
+    
+    if not users.updateFunds(availFunds + (amount * price)):
+        print ("Can't update DB funds" )
+        return "fail" 
+    
+    database.runQuery("INSERT INTO sells (stockname, sellprice, amount) VALUES ('"+stockName+"','"+str(price)+"',"+str(amount)+");")
+    
+    return "success"
+    
 
 def buyData():
     return database.runQueryWithResponse("select * from buys")
@@ -37,6 +56,18 @@ def sellData():
     return database.runQueryWithResponse("select * from sells")
 
 
-
+def getStockCntByName(name):
+    resBuy = database.runQueryWithResponse("select * from buys where name='"+name+"'")
+    resSell = database.runQueryWithResponse("select * from sells where name='"+name+"'")
+    
+    cnt = 0
+    
+    for x in resBuy:
+        cnt+= int(x[2])
+    for x in resSell:
+        cnt -= int(x[2])
+    
+    return cnt
+    
 
 
